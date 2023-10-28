d4231ee4871d60f04347e6c04fc062b901e03102
Trong Tran <you@example.com>
2023-10-28 14:45:57 +0700

    update server

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,11 @@
 import {app} from './app'
 require('dotenv').config();
+import connectDB from './utils/database';
 
 //Create server
 app.listen(process.env.PORT, () => {
     console.log(`Server is connected with port ${process.env.PORT}`);
+
+    connectDB();    
 })
+
