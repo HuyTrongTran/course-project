82df87283349496f26c085e35bcb51e04fe7443b
Trong Tran <you@example.com>
2023-11-06 22:20:42 +0700

    update user

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -39,7 +39,7 @@ const userSchema: Schema<IUser> = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: [true, "Please enter your password"], 
+        // required: [true, "Please enter your password"], 
         minlength: [6, "Password must be at least 6 characters"],
         select: false
     },
@@ -73,12 +73,16 @@ userSchema.pre<IUser>('save', async function (next) {
 
 // Sign access token
 userSchema.methods.SignAccessToken = function() {
-    return jwt.sign({id: this._id}, process.env.ACCESS_TOKEN || '')
+    return jwt.sign({id: this._id}, process.env.ACCESS_TOKEN || '', {
+        expiresIn: "5m"
+    })
 }
 
 // Sign refresh token
 userSchema.methods.SignRefreshToken = function() {
-    return jwt.sign({id: this._id}, process.env.REFRESH_TOKEN || '')
+    return jwt.sign({id: this._id}, process.env.REFRESH_TOKEN || '', {
+        expiresIn: "3d"
+    })
 }
 
 // Compare password
--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 import { Response } from "express";
 import { IUser } from "../models/user.model";
 import { redis } from "./redis";
+import exp from "constants";
 
 interface ITokenOptions {
     expires: Date;
@@ -9,6 +10,23 @@ interface ITokenOptions {
     httpOnly: boolean;
     sameSite: 'lax' | 'strict' |'none' | undefined;
     secure?: boolean;
+}
+// parse environment variables to intergrates with fallback
+const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || '300', 10);
+const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '1200', 10);
+
+// options for cookie
+export const accessTokenOptions : ITokenOptions ={
+    expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+    maxAge: accessTokenExpire * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'lax',
+}
+export const refreshTokenOptions : ITokenOptions ={
+    expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+    maxAge: refreshTokenExpire * 24 * 60 * 60 *1000,
+    httpOnly: true,
+    sameSite: 'lax',
 }
 
 export const sendToken = (user: IUser, statusCode: number, res: Response) => {
@@ -18,23 +36,6 @@ export const sendToken = (user: IUser, statusCode: number, res: Response) => {
     //upload session to redis
     redis.set(user._id, JSON.stringify(user) as any);
 
-    // parse environment variables to intergrates with fallback
-    const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || '300', 10);
-    const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '1200', 10);
-
-    // options for cookie
-    const accessTokenOptions : ITokenOptions ={
-        expires: new Date(Date.now() + accessTokenExpire * 1000),
-        maxAge: accessTokenExpire *1000,
-        httpOnly: true,
-        sameSite: 'lax',
-    }
-    const refreshTokenOptions : ITokenOptions ={
-        expires: new Date(Date.now() + refreshTokenExpire * 1000),
-        maxAge: refreshTokenExpire *1000,
-        httpOnly: true,
-        sameSite: 'lax',
-    }
 
     // only set secure to true in production
     if(process.env.NODE_ENV === 'production'){
