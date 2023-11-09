0e6bc19f40a334d7e97a97dc7976dbda6c509bcc
Nguyen Hoang Minh Triet <nguyenhoangminhtriet10a2@gmail.com>
2023-11-09 20:54:35 +0700

    update lan 1

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updatePassword, updateProfilePicture, updateUserInfo } from '../controllers/user.controller';
 import { isAuthenticated } from '../middleware/auth';
+
+
 const userRouter = express.Router();
 
 userRouter.post('/registration', registrationUser)
@@ -9,7 +11,7 @@ userRouter.post('/activate-user', activateUser)
 
 userRouter.post('/login', loginUser)
 
-userRouter.get('/logout',isAuthenticated, logoutUser)
+userRouter.get('/logout', isAuthenticated, logoutUser)
 
 userRouter.get("/refresh", updateAccessToken)
 
@@ -17,9 +19,10 @@ userRouter.get("/me", isAuthenticated, getUserInfo)
 
 userRouter.post("/social-auth", socialAuth)
 
-userRouter.put("/update-user-info",isAuthenticated, updateUserInfo)
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo)
 
-userRouter.put("/update-user-password",isAuthenticated, updatePassword)
+userRouter.put("/update-user-password", isAuthenticated, updatePassword)
 
-userRouter.put("/update-user-avatar",isAuthenticated, updateProfilePicture)
-export default userRouter
+userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture)
+
+export default userRouter;
