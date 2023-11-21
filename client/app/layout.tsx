7094a9afe21479fbac0048ddbdd8baadd0666272
Trong Tran <you@example.com>
2023-11-21 14:42:35 +0700

    update login

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,10 +1,17 @@
-'use client'
+"use client";
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import { Josefin_Sans } from "next/font/google";
 import { ThemeProvider } from "./utils/them-provider";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "./Provider";
+import { SessionProvider } from "next-auth/react";
+import React, { FC, useEffect } from "react";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+import Loader from "./components/Loader/Loader";
+import socketIO from "socket.io-client";
+const ENDPOINT = process.env.NEXT_PUBLIC_SOCKET_SERVER_URI || "";
+const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,17 +31,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body
-        className={`${poppins.variable} ${josefin.variable} !bg-white dark:bg-gradient-to-b dark: from-gray-900 dark:to-black duration-300 `}
+        className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}
       >
         <Providers>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {children}
-            <Toaster position="top-center" reverseOrder={false} />
-          </ThemeProvider>
+          <SessionProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <Custom>
+                <div>{children}</div>
+              </Custom>
+              <Toaster position="top-center" reverseOrder={false} />
+            </ThemeProvider>
+          </SessionProvider>
         </Providers>
       </body>
     </html>
   );
 }
+
+const Custom: FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isLoading } = useLoadUserQuery({});
+
+  useEffect(() => {
+    socketId.on("connection", () => {});
+  }, []);
+
+  return <div>{isLoading ? <Loader /> : <div>{children} </div>}</div>;
+};
--- client/redux/store.ts
+++ client/redux/store.ts
@@ -11,3 +11,12 @@ export const store = configureStore({
     devTools: false,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
 })
+
+// Call the refresh
+const initialApp = async () => {
+    await store.dispatch(apiSlice.endpoints.refreshToken.initiate({}, {forceRefetch: true}))
+
+    await store.dispatch(apiSlice.endpoints.loadUser.initiate({}, {forceRefetch: true}))
+}
+
+initialApp()
